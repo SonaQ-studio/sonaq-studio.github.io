805139131d061fdd9bbd5462d55d8b3311bc017e
leopold <galileopold7@gmail.com>
2026-08-19 07:24:44 +0300

    Add single Белые джедаи (21 Aug): cover, lyrics, bright blurb, smartlink stub before Порог.

--- js/site.js
+++ js/site.js
@@ -89,6 +89,7 @@
 
   /** Единый каталог релизов (для карточек «другие» и т.п.) */
   var RELEASES = [
+    { id: "belye-dzhedai", title: "Белые джедаи", href: "belye-dzhedai.html" },
     { id: "porog", title: "Порог (альбом)", href: "porog.html" },
     { id: "pelmeni", title: "Пельмени", href: "pelmeni.html" },
     { id: "skazochnyy-memolog", title: "Сказочный мемолог", href: "skazochnyy-memolog.html" },
